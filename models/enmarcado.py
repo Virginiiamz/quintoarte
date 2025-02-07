bb51d6f31ab461d32c460f7d1987756e8c5da2b1
nicolaimachado <xnschsch957@ieshnosmachado.org>
2025-02-07 12:51:05 +0100

    creado el workflow de enmarcado

--- models/enmarcado.py
+++ models/enmarcado.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
+
 
 
 class enmarcado (models.Model):
@@ -10,7 +13,22 @@ class enmarcado (models.Model):
     precio_total = fields.Float("Precio total", required=True, readonly=False)
     fecha_enmarcado = fields.Date("Fecha Enmarcado", required=True, readonly=False)
     
+    estadoEnmarcado = fields.Selection([('enproceso','En proceso'),
+                                ('terminado','Terminado')], 'Estado', default='enproceso', required=True)
+    
 
     marco_id = fields.Many2one("quintoarte.marco",string="Marco")
     cuadro_id = fields.Many2one("quintoarte.cuadro",string="Cuadro")
     cliente_id = fields.Many2one("quintoarte.cliente",string="Cliente")
+
+    def btn_submit_to_terminado(self):
+            if not self.estadoEnmarcado:
+                raise UserError("El campo 'estadoEnmarcado' está vacío o no válido.")
+            if self.estadoEnmarcado == 'terminado':
+                raise UserError("El enmarcado ya está marcado como 'Terminado'.")
+            self.write({'estadoEnmarcado': 'terminado'})
+
+    def btn_submit_to_enproceso(self):
+        if self.estadoEnmarcado == 'enproceso':
+            raise UserError("El enmarcado ya está marcado como 'En proceso'.")
+        self.write({'estadoEnmarcado': 'enproceso'})
